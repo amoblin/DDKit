9a6e4348012d8bff8e94ca6ed18743b4f9778b7a
amoblin@mbp <amoblin@gmail.com>
2016-08-17 06:45:26 +0800

    Initial commit with demo

---
+++ DDKitDemo/DDKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDKitDemo
+//
+//  Created by amoblin on 16/8/17.
+//  Copyright © 2016年 marboo.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDKitDemo/DDKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDKitDemo
+//
+//  Created by amoblin on 16/8/17.
+//  Copyright © 2016年 marboo.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
